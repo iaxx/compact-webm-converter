f7c16912c367e021b76c65fed0b8069a1ba76b43
iaxx <iaxx233@gmail.com>
2023-06-27 18:02:20 +0300

    refactored into cleaner code

--- converter.py
+++ converter.py
@@ -5,183 +5,172 @@ import json
 import subprocess
 import threading
 import re
+from typing import Tuple
 
-# Path to ffmpeg and ffprobe executables
-ffmpeg_path = os.path.join(os.getcwd(), 'ffmpeg.exe')
-ffprobe_path = os.path.join(os.getcwd(), 'ffprobe.exe')
 
-# Desired output file size in MB
-file_size_mb = 3
+class WebmConverter:
+    def __init__(self, root: Tk, ffmpeg_path: str, ffprobe_path: str, file_size_mb: int = 3):
+        self.root = root
+        self.ffmpeg_path = ffmpeg_path
+        self.ffprobe_path = ffprobe_path
+        self.file_size_mb = file_size_mb
+        self.file_path = None
+        self.output_file = None
+        self.duration_s = 0
+        self.conversion_in_progress = False
+        self.halve_resolution = False  # Flag to check if resolution should be halved
+        self.target_framerate = 30  # Default target framerate
 
-# Global variables
-file_path = None
-output_file = None
-duration_s = 0
-conversion_in_progress = False
-halve_resolution = False  # Flag to check if resolution should be halved
-target_framerate = 30  # Default target framerate
+        self.label = Label(self.root, text="Place the file here")
+        self.label.pack()
 
-def get_video_duration(input_file):
-    command = f'"{ffprobe_path}" -v quiet -print_format json -show_format -show_streams "{input_file}"'
-    output = subprocess.check_output(command, shell=True, text=True)
-    video_data = json.loads(output)
-    duration_s = float(video_data['format']['duration'])
-    return duration_s
+        self.browse_button = Button(self.root, text="Browse", command=self.browse_file)
+        self.browse_button.pack(pady=10)
 
-def get_video_resolution(input_file):
-    command = f'"{ffprobe_path}" -v error -select_streams v:0 -show_entries stream=width,height -of json "{input_file}"'
-    output = subprocess.check_output(command, shell=True, text=True)
-    video_data = json.loads(output)
-    width = video_data['streams'][0]['width']
-    height = video_data['streams'][0]['height']
-    return width, height
+        self.progress_var = StringVar()
+        self.progress_var.set('0')
+        self.progress_bar = Progressbar(self.root, length=400, mode='determinate', variable=self.progress_var)
+        self.progress_bar.pack(pady=20)
 
-def calculate_bitrate(file_size_mb, duration_s):
-    file_size_kb = file_size_mb * 1024
-    bitrate_k = file_size_kb * 8 / duration_s
-    return bitrate_k
+        self.resolution_frame = LabelFrame(self.root, text="Resolution")
+        self.resolution_frame.pack(pady=5)
 
-def convert_to_webm(input_file, output_file, duration_s, halve_resolution, target_framerate):
-    global conversion_in_progress
+        self.half_res_button = Button(self.resolution_frame, text="Half res", command=self.set_half_resolution)
+        self.half_res_button.pack(side="left", padx=5)
 
-    # Disable the "Go" button during conversion
-    go_button.config(state='disabled')
+        self.full_res_button = Button(self.resolution_frame, text="Full res", command=self.set_full_resolution)
+        self.full_res_button.pack(side="left", padx=5)
 
-    # Update the message label
-    message_label.config(text="Conversion in progress, please wait...")
+        self.framerate_frame = LabelFrame(self.root, text="Framerate")
+        self.framerate_frame.pack(pady=5)
 
-    # Delete existing output file if it exists
-    if os.path.exists(output_file):
-        os.remove(output_file)
+        self.framerate_30_button = Button(self.framerate_frame, text="30", command=self.set_framerate_30)
+        self.framerate_30_button.pack(side="left", padx=5)
 
-    try:
-        width, height = get_video_resolution(input_file)
-        if halve_resolution:
-            width //= 2
-            height //= 2
-        scale_option = f"scale={width}:{height}"
+        self.framerate_60_button = Button(self.framerate_frame, text="60", command=self.set_framerate_60)
+        self.framerate_60_button.pack(side="left", padx=5)
 
-        bitrate_k = calculate_bitrate(file_size_mb, duration_s) * 0.96
-        command = f'"{ffmpeg_path}" -i "{input_file}" -c:v vp9 -b:v {bitrate_k}k -vf "{scale_option},fps=fps={target_framerate}" -pass 1 -an -f webm NUL && "{ffmpeg_path}" -i "{input_file}" -c:v vp9 -b:v {bitrate_k}k -vf "{scale_option},fps=fps={target_framerate}" -pass 2 -an -f webm "{output_file}"'
-        process = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, universal_newlines=True)
-        regex = re.compile(r'time=(\d+:\d+:\d+.\d+)')
-        while True:
-            line = process.stderr.readline()
-            if line == '' and process.poll() is not None:
-                break
-            if line:
-                matches = regex.findall(line)
-                if matches:
-                    time_str = matches[0]
-                    h, m, s = map(float, time_str.split(':'))
-                    time_s = h * 3600 + m * 60 + s
-                    progress_var.set(int((time_s / duration_s) * 100))
+        self.go_button = Button(self.root, text="Start", command=self.start_conversion)
+        self.go_button.pack(pady=10)
 
-        # Wait for the process to terminate
-        process.communicate()
+        self.message_label = Label(self.root, text="")
+        self.message_label.pack()
 
-    except KeyboardInterrupt:
-        print("Conversion interrupted.")
-    else:
-        if process.returncode != 0:
-            print(f'Error converting {input_file} to {output_file}')
+    def get_video_duration(self, input_file: str) -> float:
+        command = f'"{self.ffprobe_path}" -v quiet -print_format json -show_format -show_streams "{input_file}"'
+        output = subprocess.check_output(command, shell=True, text=True)
+        video_data = json.loads(output)
+        duration_s = float(video_data['format']['duration'])
+        return duration_s
+
+    def get_video_resolution(self, input_file: str) -> Tuple[int, int]:
+        command = f'"{self.ffprobe_path}" -v error -select_streams v:0 -show_entries stream=width,height -of json "{input_file}"'
+        output = subprocess.check_output(command, shell=True, text=True)
+        video_data = json.loads(output)
+        width = video_data['streams'][0]['width']
+        height = video_data['streams'][0]['height']
+        return width, height
+
+    def calculate_bitrate(self, duration_s: float) -> float:
+        file_size_kb = self.file_size_mb * 1024
+        bitrate_k = file_size_kb * 8 / duration_s
+        return bitrate_k
+
+    def convert_to_webm(self, input_file: str, output_file: str, duration_s: float):
+        # Disable the "Go" button during conversion
+        self.go_button.config(state='disabled')
+
+        # Update the message label
+        self.message_label.config(text="Conversion in progress, please wait...")
+
+        # Delete existing output file if it exists
+        if os.path.exists(output_file):
+            os.remove(output_file)
+
+        try:
+            width, height = self.get_video_resolution(input_file)
+            if self.halve_resolution:
+                width //= 2
+                height //= 2
+            scale_option = f"scale={width}:{height}"
+
+            bitrate_k = self.calculate_bitrate(duration_s) * 0.96
+            command = f'"{self.ffmpeg_path}" -i "{input_file}" -c:v vp9 -b:v {bitrate_k}k -vf "{scale_option},fps=fps={self.target_framerate}" -pass 1 -an -f webm NUL && "{self.ffmpeg_path}" -i "{input_file}" -c:v vp9 -b:v {bitrate_k}k -vf "{scale_option},fps=fps={self.target_framerate}" -pass 2 -an -f webm "{output_file}"'
+            process = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, universal_newlines=True)
+            regex = re.compile(r'time=(\d+:\d+:\d+.\d+)')
+            while True:
+                line = process.stderr.readline()
+                if line == '' and process.poll() is not None:
+                    break
+                if line:
+                    matches = regex.findall(line)
+                    if matches:
+                        time_str = matches[0]
+                        h, m, s = map(float, time_str.split(':'))
+                        time_s = h * 3600 + m * 60 + s
+                        self.progress_var.set(int((time_s / duration_s) * 100))
+
+            # Wait for the process to terminate
+            process.communicate()
+
+        except KeyboardInterrupt:
+            print("Conversion interrupted.")
         else:
-            print(f'Successfully converted {input_file} to {output_file}')
+            if process.returncode != 0:
+                print(f'Error converting {input_file} to {output_file}')
+            else:
+                print(f'Successfully converted {input_file} to {output_file}')
 
-    # Enable the "Go" button after conversion is finished
-    go_button.config(state='normal')
-    conversion_in_progress = False
+        # Enable the "Go" button after conversion is finished
+        self.go_button.config(state='normal')
+        self.conversion_in_progress = False
 
-    # Update the message label
-    message_label.config(text="Conversion completed.")
+        # Update the message label
+        self.message_label.config(text="Conversion completed.")
 
+    def browse_file(self):
+        self.file_path = filedialog.askopenfilename(filetypes=(("MP4 files", "*.mp4"), ("MKV files", "*.mkv"), ("All files", "*.*")))
+        if self.file_path:
+            self.output_file = self.file_path.rsplit('.', 1)[0] + '.webm'
+            self.duration_s = self.get_video_duration(self.file_path)
+            self.label.config(text="Selected file: " + os.path.basename(self.file_path))  # Display selected filename
 
-    # Enable the "Go" button after conversion is finished
-    go_button.config(state='normal')
-    conversion_in_progress = False
+    def start_conversion(self):
+        if self.conversion_in_progress:
+            return  # Skip if a conversion is already in progress
 
-    # Update the message label
-    message_label.config(text="Conversion completed.")
+        self.conversion_in_progress = True
+        threading.Thread(target=self.convert_to_webm, args=(self.file_path, self.output_file, self.duration_s)).start()
 
-def browse_file():
-    global file_path, output_file, duration_s
-    file_path = filedialog.askopenfilename(filetypes=(("MP4 files", "*.mp4"), ("MKV files", "*.mkv"), ("All files", "*.*")))
-    if file_path:
-        output_file = file_path.rsplit('.', 1)[0] + '.webm'
-        duration_s = get_video_duration(file_path)
-        label.config(text="Selected file: " + os.path.basename(file_path))  # Display selected filename
+    def set_half_resolution(self):
+        self.halve_resolution = True
+        self.half_res_button.config(state='disabled')
+        self.full_res_button.config(state='normal')
 
-def start_conversion():
-    global conversion_in_progress, halve_resolution, target_framerate
+    def set_full_resolution(self):
+        self.halve_resolution = False
+        self.full_res_button.config(state='disabled')
+        self.half_res_button.config(state='normal')
 
-    if conversion_in_progress:
-        return  # Skip if a conversion is already in progress
+    def set_framerate_30(self):
+        self.target_framerate = 30
+        self.framerate_30_button.config(state='disabled')
+        self.framerate_60_button.config(state='normal')
 
-    conversion_in_progress = True
-    threading.Thread(target=convert_to_webm, args=(file_path, output_file, duration_s, halve_resolution, target_framerate)).start()
+    def set_framerate_60(self):
+        self.target_framerate = 60
+        self.framerate_60_button.config(state='disabled')
+        self.framerate_30_button.config(state='normal')
 
-def set_half_resolution():
-    global halve_resolution
-    halve_resolution = True
-    half_res_button.config(state='disabled')
-    full_res_button.config(state='normal')
-
-def set_full_resolution():
-    global halve_resolution
-    halve_resolution = False
-    full_res_button.config(state='disabled')
-    half_res_button.config(state='normal')
-
-def set_framerate_30():
-    global target_framerate
-    target_framerate = 30
-    framerate_30_button.config(state='disabled')
-    framerate_60_button.config(state='normal')
-
-def set_framerate_60():
-    global target_framerate
-    target_framerate = 60
-    framerate_60_button.config(state='disabled')
-    framerate_30_button.config(state='normal')
 
 root = Tk()
 root.title('3mb webm converter')
 root.geometry('490x340')
 root.minsize(490, 340)
 
-label = Label(root, text="Place the file here")
-label.pack()
+ffmpeg_path = os.path.join(os.getcwd(), 'ffmpeg.exe')
+ffprobe_path = os.path.join(os.getcwd(), 'ffprobe.exe')
 
-browse_button = Button(root, text="Browse", command=browse_file)
-browse_button.pack(pady=10)
-
-progress_var = StringVar()
-progress_var.set('0')
-progress_bar = Progressbar(root, length=400, mode='determinate', variable=progress_var)
-progress_bar.pack(pady=20)
-
-resolution_frame = LabelFrame(root, text="Resolution")
-resolution_frame.pack(pady=5)
-
-half_res_button = Button(resolution_frame, text="Half res", command=set_half_resolution)
-half_res_button.pack(side="left", padx=5)
-
-full_res_button = Button(resolution_frame, text="Full res", command=set_full_resolution)
-full_res_button.pack(side="left", padx=5)
-
-framerate_frame = LabelFrame(root, text="Framerate")
-framerate_frame.pack(pady=5)
-
-framerate_30_button = Button(framerate_frame, text="30", command=set_framerate_30)
-framerate_30_button.pack(side="left", padx=5)
-
-framerate_60_button = Button(framerate_frame, text="60", command=set_framerate_60)
-framerate_60_button.pack(side="left", padx=5)
-
-go_button = Button(root, text="Start", command=start_conversion)
-go_button.pack(pady=10)
-
-message_label = Label(root, text="")
-message_label.pack()
+app = WebmConverter(root, ffmpeg_path, ffprobe_path)
 
 root.mainloop()
